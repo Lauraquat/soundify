9936d9081ca7e1e769cc3814c523d2650b75fb7f
Lauraquat <lauraquat@gmail.com>
2022-05-27 09:53:06 +0200

    ajout entity artiste + migration

---
+++ src/Entity/Artiste.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArtisteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArtisteRepository::class)]
+class Artiste
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id = "0";
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name = "anonymous";
+
+    #[ORM\OneToOne(inversedBy: 'artiste', targetEntity: User::class, cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private $user;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
