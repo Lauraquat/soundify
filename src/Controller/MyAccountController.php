ccb56ddf83aa799ea5a48100eab0333832c47745
Lauraquat <lauraquat@gmail.com>
2022-05-27 11:41:16 +0200

    création formulaire modif mot de passe dans account

--- src/Controller/MyAccountController.php
+++ src/Controller/MyAccountController.php
@@ -4,17 +4,37 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security as UserSecurity;
 
 class MyAccountController extends AbstractController
 {
     #[Route('/account', name: 'app_my_account')]
     #[Security('is_granted("ROLE_USER")')]
-    public function index(): Response
+    public function index(UserSecurity $security): Response
     {
+        $user = $security->getUser();
+
+        $formBuilder = $this->createFormBuilder($user);
+        $formBuilder
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => false,
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Répéter le mot de passe']
+            ])
+            ->add('artiste', TextType::class, ['label' => "Nom de l'artiste"])
+            ->add('submit', SubmitType::class, ['label' => 'Mettre à jour']);
+
+        $form = $formBuilder->getForm();
+
         return $this->render('my_account/index.html.twig', [
-            'controller_name' => 'MyAccountController',
+            'form' => $form->createView(),
         ]);
     }
 }
